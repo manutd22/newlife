5e23bc5cc8d737efaf1d6ebea9905cc909acc55f
artem <vadis1766@gmail.com>
2024-09-13 19:44:14 +0300

    third

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactj/', // Измените это на имя вашего репозитория
+  base: '/newlife/', // Измените это на имя вашего репозитория
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
